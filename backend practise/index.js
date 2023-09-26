c284c7bc6b29e5a06e245ea3a25171b3e73558a5
ankush3681 <ankushkumar83623@gmail.com>
2023-09-26 17:31:12 +0530

    post data

--- backend practise/index.js
+++ backend practise/index.js
@@ -1,8 +1,15 @@
 const express = require("express");
 const app = express();
 
+app.use(express.json());
+
 app.get("/",(req,res)=>{
-    res.send("Hello there")
+    res.status(200).send("Hello there")
+})
+
+app.post("/addnew",(req,res)=>{
+    res.send("data is added")
+    console.log(req.body)
 })
 
 app.listen(5000)
