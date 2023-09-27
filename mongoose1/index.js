2360207d72785da492634e3d7995393d10c8b03b
ankush3681 <ankushkumar83623@gmail.com>
2023-09-27 11:39:12 +0530

    schema and model

--- mongoose1/index.js
+++ mongoose1/index.js
@@ -2,13 +2,35 @@ const mongoose = require("mongoose");
 
 const main = async () =>{
 try {
-    const connection = await mongoose.connect("mongodb://127.0.0.1:27017");
+    const connection = await mongoose.connect("mongodb://127.0.0.1:27017/practise_user");
     console.log("connected to db");
+    // await UserModel.insertMany([{name:"ankus",age:23,married:false},{name:"Rahul",age:25,married:true}]);
+    console.log("inserted data")
+    const user = new UserModel({
+        name:"Vikas",
+        age:25,
+        married:false
+    })
+    await user.save();
 } catch (err) {
     console.log("cannot connect to db");
     console.log(err);
 }
     
 }
+
+const UserSchema = mongoose.Schema({
+    name:String,
+    age:Number,
+    married:Boolean
+},
+{
+    versionKey:false
+})
+
+const UserModel = mongoose.model("user",UserSchema);
+
+
+
 
 main();
