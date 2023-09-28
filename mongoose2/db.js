e676db571d31933cc8d9b2090287d20447728bad
ankush3681 <ankushkumar83623@gmail.com>
2023-09-28 15:45:22 +0530

    dsahg

--- mongoose2/db.js
+++ mongoose2/db.js
@@ -2,6 +2,19 @@ const mongoose = require("mongoose");
 
 const connection = mongoose.connect("mongodb://127.0.0.1:27017/mongoose2");
 
+const UserSchema = mongoose.Schema({
+    name:{type:String,required:true},
+    age:{type:Number,required:true},
+    city:{type:String,required:true},
+    language:{type:String,required:true}
+},{
+    versionKey:false
+})
+
+const UserModel = mongoose.model("user",UserSchema);
+
+
 module.exports={
-    connection
+    connection,
+    UserModel
 }
--- mongoose2/index.js
+++ mongoose2/index.js
@@ -1,11 +1,28 @@
 const express = require("express");
-const {connection} = require("./db")
+const {connection,UserModel} = require("./db")
 
-const app = express();
+const app = express(); 
 
-app.get("/",(req,res)=>{
-    res.status(200).send("Hello this is home page")
+app.use(express.json());
+// create
+
+app.post("/adduser",async (req,res)=>{
+ const data = await UserModel(req.body);
+ await data.save();
+ res.status(200).send("data is added")
 })
+
+// read
+
+app.get("/",async(req,res)=>{
+    const user = await UserModel.find();
+    res.status(200).send(user)
+})
+
+
+// update
+
+// app.update("/")
 
 app.listen(3200, async()=>{
     try {
