01c01af8385bcd6965440d3589a356ce49fbf402
ankush3681 <ankushkumar83623@gmail.com>
2023-09-29 10:51:52 +0530

    crud opration done

--- mongoose2/index.js
+++ mongoose2/index.js
@@ -1,36 +1,64 @@
 const express = require("express");
-const {connection,UserModel} = require("./db")
+const { connection, UserModel } = require("./db");
 
-const app = express(); 
+const app = express();
 
 app.use(express.json());
 // create
 
-app.post("/adduser",async (req,res)=>{
- const data = await UserModel(req.body);
- await data.save();
- res.status(200).send("data is added")
-})
+app.post("/adduser", async (req, res) => {
+  try {
+    const data = new UserModel(req.body);
+    await data.save();
+    res.status(200).send("data is added");
+  } catch (err) {
+    res.send(err);
+  }
+});
 
 // read
 
-app.get("/",async(req,res)=>{
-    const user = await UserModel.find();
-    res.status(200).send(user)
-})
-
+app.get("/", async (req, res) => {
+  const query = req.query;
+  try {
+    const user = await UserModel.find(query);
+    res.status(200).send(user);
+  } catch (err) {
+    res.send(err);
+  }
+});
 
 // update
 
-// app.update("/")
+app.patch("/update/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    await UserModel.findByIdAndUpdate({ _id: id }, req.body);
+    res.status(200).send("data updated");
+  } catch (err) {
+    res.send(err);
+  }
+});
 
-app.listen(3200, async()=>{
-    try {
-        await connection;
-        console.log("server is connected to DB");
-    } catch (err) {
-        console.log(err);
-        console.log("cannot connect to DB");
-    }
-    console.log("server is running on port 3200");
-})
+// Delete
+
+app.delete("/deleteuser/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    await UserModel.findByIdAndDelete({ _id: id });
+    res.status(200).send("User Deleted Successfully");
+  } catch (err) {
+    res.send(err);
+  }
+});
+
+app.listen(3200, async () => {
+  try {
+    await connection;
+    console.log("server is connected to DB");
+  } catch (err) {
+    console.log(err);
+    console.log("cannot connect to DB");
+  }
+  console.log("server is running on port 3200");
+});
