9761adda9f4517a4f19defbf158c89fd36fc8cb2
ankush3681 <ankushkumar83623@gmail.com>
2023-09-30 16:59:50 +0530

    done

--- mongoose3/index.js
+++ mongoose3/index.js
@@ -5,11 +5,54 @@ const app = express();
 
 app.use(express.json());
 
+// create
 
-app.get("/user",(req,res)=>{
-    res.send("hello there")
+app.post("/adduser", async(req,res)=>{
+try {
+    const user = new UserModel(req.body);
+    await user.save();
+    res.status(200).send({"mssg":"new user added successfully"});
+} catch (err) {
+    res.status(200).send(err);
+}
 })
 
+// read
+
+app.get("/user", async(req,res)=>{
+    try {
+        const user = await UserModel.find();
+        res.send(user)
+    } catch (err) {
+        res.send(err);
+    }
+})
+
+
+// update
+
+app.patch("/update/:id", async(req,res)=>{
+    const {id} = req.params;
+try {
+    await UserModel.findByIdAndUpdate({_id:id},req.body);
+    res.send("User data updated successdully")
+} catch (err) {
+    res.send(err)
+}
+})
+
+
+// delete
+
+app.delete("/delete/:id", async(req,res)=>{
+    const {id} = req.params;
+    try {
+        await UserModel.findByIdAndDelete({_id:id});
+        res.send('data deleted')
+    } catch (err) {
+        res.send(err);
+    }
+})
 app.listen(3000, async ()=>{
     try {
         await connection 
