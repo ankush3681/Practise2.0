dfa665aed307e790a58e819c1fdc7214a91c10ea
ankush3681 <ankushkumar83623@gmail.com>
2023-10-02 23:04:18 +0530

    dsg

--- auth/index.js
+++ auth/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const { connection } = require("./db");
 const { UserRouter } = require("./route/User.route");
+const jwt = require("jsonwebtoken");
 require("dotenv").config();
 
 const app = express();
@@ -12,6 +13,26 @@ app.use("/user",UserRouter);
 app.get("/",(req,res)=>{
     res.send("Home Page")
 })
+
+// protected route
+
+app.get("/cart",(req,res)=>{
+    const {token} = req.query;
+    var decoded = jwt.verify(token, 'secret_message');
+    if(decoded){
+        res.status(200).send("Welcome to cart");
+    }else{
+        res.status(400).send({"msg":"Jwt required"});
+    }
+    // jwt.verify(token, 'secret_message', function(err, decoded) {
+    //     console.log(decoded)
+    //     if(decoded){
+    //         res.status(200).send("Welcome to cart");
+    //     }else{
+    //         res.status(400).send({"msg":err.message});
+    //     }      });
+})
+
 
 app.listen(process.env.Port,async()=>{
     try {
--- auth/route/User.route.js
+++ auth/route/User.route.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const { UserModel } = require("../model/user.model");
-
+const jwt = require('jsonwebtoken');
 const UserRouter = express.Router();
 
 
@@ -8,17 +8,25 @@ UserRouter.post("/register",async(req,res)=>{
    try {
     const user = new UserModel(req.body);
     await user.save();
-    res.status(200).json({"msg":"New user Registered successfully"});
+    res.status(200).json("New user Registered successfully");
    } catch (err) {
     res.send(err);
    }
 })
 
 UserRouter.post("/login", async(req,res)=>{
+    const {email,pass} = req.body;
     try {
-        
+        const user = await UserModel.findOne({email,pass});
+        if(user){
+            const token = jwt.sign({ payload:"kuch bhi" }, 'secret_message');
+
+            res.status(200).send({"msg":"Login Successfull.",token});
+        }else{
+            res.status(200).send({"msg":"Wrong Credentials!!"});;
+        }
     } catch (err) {
-        
+        res.status(400).send({"msg":err.message});
     }
 })
 
